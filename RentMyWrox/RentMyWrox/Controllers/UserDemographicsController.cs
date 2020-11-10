ee46b054a09b44e924b1bc22e4e42f5e2c089d8e
Goutam <Goutamnrg@gmail.com>
2020-11-10 17:24:08 -0500

    Chapter 08, 01

---
+++ RentMyWrox/RentMyWrox/Controllers/UserDemographicsController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentMyWrox.Models;
+
+namespace RentMyWrox.Controllers
+{
+    public class UserDemographicsController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Default/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Default/Create
+        public ActionResult Create()
+        {
+            return View("Manage");
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        public ActionResult Create(UserDemographics obj)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = new UserDemographics
+            {
+                Gender = "Male",
+                Birthdate = new DateTime(2000, id, id),
+                MaritalStatus = "Married",
+                OwnHome = true,
+                TotalPeopleInHome = id,
+                Hobbies = new List<string> { "Gardening", "Other" }
+            };
+            return View("Manage", model);
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
